f7a1e1311d6f21cf1a522aad562fbbb1131548cc
blendspace <blend_space@outlook.com>
2024-07-14 19:12:15 +0800

    vbo/ebo使用

--- app/src/main/java/com/blendspace/example/myglsurfaceview/Triangle.kt
+++ app/src/main/java/com/blendspace/example/myglsurfaceview/Triangle.kt
@@ -22,9 +22,10 @@ class Triangle {
             "}"
 
     private val triangleCoords = floatArrayOf(
-        0.0f, 0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f
+        -0.5f, 0.5f, 0.0f, // 左上角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        0.5f, -0.5f, 0.0f, // 右下角
+        0.5f, 0.5f, 0.0f // 右上角
     )
 
     private var triangleMatrix = FloatArray(16)
@@ -39,6 +40,10 @@ class Triangle {
 
     private lateinit var vertexBuffer: FloatBuffer
     private var mTMatrixHandle: Int = 0;
+    private val vboIds = IntArray(1) // vbo
+    private val eboIds = IntArray(1) // ebo
+
+    private var indics = intArrayOf(0, 1, 2, 2, 3, 0)
 
     private fun loadShader(type: Int, shaderCode: String): Int {
         val shader = GLES30.glCreateShader(type)
@@ -69,17 +74,38 @@ class Triangle {
 
         GLES30.glLinkProgram(mProgram)
 
+        // vbo使用 vbo需要在link之后使用
+        GLES30.glGenBuffers(1, vboIds, 0)
+        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, vboIds[0])
+        GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, bb.capacity(), bb, GLES30.GL_STATIC_DRAW)
+        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, 0)
+
+
+        var idxBuffer = ByteBuffer.allocateDirect(indics.size * 4).order(ByteOrder.nativeOrder()).asIntBuffer()
+        idxBuffer.put(indics).position(0)
+        // ebo使用
+        GLES30.glGenBuffers(1, eboIds, 0)
+        GLES30.glBindBuffer(GLES30.GL_ELEMENT_ARRAY_BUFFER, eboIds[0])
+        GLES30.glBufferData(GLES30.GL_ELEMENT_ARRAY_BUFFER, idxBuffer.capacity() * 4, idxBuffer, GLES30.GL_STATIC_DRAW)
+        GLES30.glBindBuffer(GLES30.GL_ELEMENT_ARRAY_BUFFER, 0)
+
+
+
+
     }
 
     fun draw() {
         // 使用program
         GLES30.glUseProgram(mProgram)
 
+        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, vboIds[0])
+        GLES30.glBindBuffer(GLES30.GL_ELEMENT_ARRAY_BUFFER, eboIds[0])
 
         // 将数据传递给shader
         mPositionHandle = GLES30.glGetAttribLocation(mProgram, "vPosition")
         GLES30.glEnableVertexAttribArray(mPositionHandle)
-        GLES30.glVertexAttribPointer(mPositionHandle, 3, GLES30.GL_FLOAT, false, 0, vertexBuffer)
+//        GLES30.glVertexAttribPointer(mPositionHandle, 3, GLES30.GL_FLOAT, false, 0, vertexBuffer)
+        GLES30.glVertexAttribPointer(mPositionHandle, 3, GLES30.GL_FLOAT, false, 0, 0)
 
         mColorHandle = GLES30.glGetUniformLocation(mProgram, "vColor")
         GLES30.glUniform4fv(mColorHandle, 1, color, 0)
@@ -88,8 +114,12 @@ class Triangle {
         GLES30.glUniformMatrix4fv(mTMatrixHandle, 1, false, triangleMatrix, 0)
 
         // draw array， 绘制三角形
-        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 0, triangleCoords.size / 3)
+//        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 0, triangleCoords.size / 3)
+        GLES30.glDrawElements(GLES30.GL_TRIANGLES, indics.size, GLES30.GL_UNSIGNED_INT, 0)
         GLES30.glDisableVertexAttribArray(mPositionHandle)
+
+        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, 0)
+        GLES30.glBindBuffer(GLES30.GL_ELEMENT_ARRAY_BUFFER, 0)
 
 
 
